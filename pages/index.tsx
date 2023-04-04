49da94d8381f5417eeb46fda5d74cc5bfe6f694e
sgsg9447 <sgsg9447@gmail.com>
2023-04-04 19:56:55 +0900

    feat : parsedDatabaseItem

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,31 @@
 import { getDatabaseItems } from "@/cms/notionClient";
+import {
+  parseDatabaseItems,
+  ParsedDatabseItemType,
+} from "@/utils/parseDatabaseItems";
 import { GetStaticProps } from "next";
 import React from "react";
 
-function Home() {
+interface HomeProps {
+  databaseItems: ParsedDatabseItemType[];
+}
+
+function Home({ databaseItems }: HomeProps) {
+  console.log("databaseItems : >>", databaseItems);
   return <div>Home</div>;
 }
 
 export default Home;
 
-export const getStaticProps : GetStaticProps = async () =>{
- if(!process.env.DATABSE_ID) throw new Error("databaseId is not defined!!")
-  const databaseItems = await getDatabaseItems(process.env.DATABSE_ID)
-  console.log("databaseItems :>> ", databaseItems)
+export const getStaticProps: GetStaticProps<HomeProps> = async () => {
+  if (!process.env.DATABSE_ID) throw new Error("databaseId is not defined!!");
+  const databaseItems = await getDatabaseItems(process.env.DATABSE_ID);
+  // console.log("databaseItems :>> ", databaseItems)
+  const parsedDatabaseItems = parseDatabaseItems(databaseItems);
+  // console.log("parsedDatabaseItems :>> ", parsedDatabaseItems);
   return {
-    props: {}
-  }
-}
+    props: {
+      databaseItems: parsedDatabaseItems,
+    },
+  };
+};
