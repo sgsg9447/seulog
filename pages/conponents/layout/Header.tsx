3ec156f473a162ec4dc2a47349fcb7388e8e6a97
sgsg9447 <sgsg9447@gmail.com>
2023-04-04 16:00:21 +0900

    feat : footer 변경 및 header 완충제 작용

--- pages/conponents/layout/Header.tsx
+++ pages/conponents/layout/Header.tsx
@@ -14,30 +14,36 @@ const navLinks = [
 
 export default function Header() {
   return (
-    <header className="bg-white fixed top-0 w-full z-50">
-      <nav className="p-4 flex flex-row justify-between max-w-5xl mx-auto">
-        <h1 className="font-black text-4xl">
-          <Link href={"/"}>Seulog</Link>
-        </h1>
-        <ul className="flex flex-row gap-2 items-center">
-          {navLinks.map(({ name, link }) => (
-            <li key={name} className="text-gray-600 font-medium">
-              <Link href={link}>
-                <a className="p-3  hover:bg-gray-100 rounded-md hover:text-black ">{name}</a>
+    <>
+      <header className="bg-white fixed top-0 w-full z-50">
+        <nav className="p-4 flex flex-row justify-between max-w-5xl mx-auto">
+          <h1 className="font-black text-4xl">
+            <Link href={"/"}>Seulog</Link>
+          </h1>
+          <ul className="flex flex-row gap-2 items-center">
+            {navLinks.map(({ name, link }) => (
+              <li key={name} className="text-gray-600 font-medium">
+                <Link href={link}>
+                  <a className="p-3  hover:bg-gray-100 rounded-md hover:text-black ">
+                    {name}
+                  </a>
+                </Link>
+              </li>
+            ))}
+            <li>
+              <Link href={"/search"}>
+                <a>
+                  <IconButton
+                    icon={<AiOutlineSearch size={"1.5rem"} color="white" />}
+                  />
+                </a>
               </Link>
             </li>
-          ))}
-          <li>
-            <Link href={"/search"}>
-              <a>
-                <IconButton
-                  icon={<AiOutlineSearch size={"1.5rem"} color="white" />}
-                />
-              </a>
-            </Link>
-          </li>
-        </ul>
-      </nav>
-    </header>
+          </ul>
+        </nav>
+      </header>
+      <div className="h-[72px]" /> 
+      {/* 완충제 역할을 함 header의 높이가 72px이여서 이만큼 띄어준다는 의미! 이 뒤에 내용이 들어올 수 없다!  */}
+    </>
   );
 }
