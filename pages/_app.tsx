e6e9d33600a4901af167bd0d39d2841dd9944a20
sgsg9447 <sgsg9447@gmail.com>
2023-04-19 23:15:10 +0900

    feat : NotionPageRenderer 추가

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,12 @@
-import "@/styles/globals.css";
 import type { AppProps } from "next/app";
-import "pretendard/dist/web/variable/pretendardvariable.css";
 import RootLayout from "../components/layout/RootLayout";
+
+import "react-notion-x/src/styles.css";
+import "prismjs/themes/prism-tomorrow.css";
+import "katex/dist/katex.min.css";
+import "pretendard/dist/web/variable/pretendardvariable.css";
+import "@/styles/globals.css";
+
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <RootLayout>
